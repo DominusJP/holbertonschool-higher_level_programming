042458f43420f21e693036c9af0a547acab82d97
DominusJP <6775@holbertonstudents.com>
2023-09-19 08:16:10 -0700

    3.1

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for iiiidequemado in range(97, 123):
-if iiiidequemado != 113 or iiiidequemado !=101:
+if iiiidequemado != 113 and iiiidequemado !=101:
     print("{}".format(chr(iiiidequemado)), end='')
