8e7fc279afeadab913f9b9c80d5108769fda9ba5
DominusJP <6775@holbertonstudents.com>
2023-09-24 11:55:35 -0700

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,6 +5,6 @@ def element_at(my_list, idx):
     elif idx > len(my_list):
         return None
     else:
-        for i in range(idx + 1)
+        for i in range(idx + 1):
             if i == idx:
                 return my_list[i]
