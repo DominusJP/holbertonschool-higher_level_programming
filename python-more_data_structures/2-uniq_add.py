fc3cd98d36b2d8bc67349570b28bbcb818778358
DominusJP <6775@holbertonstudents.com>
2023-09-25 06:59:43 -0700

    2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     unique_set = set()
     total = 0
     for num in my_list:
-         if num not in unique_set:
+        if num not in unique_set:
             unique_set.add(num)
             total += num
     return total
