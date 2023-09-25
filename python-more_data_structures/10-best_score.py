49e8328c2cf935e5b1733da4560ff5e53b92af46
DominusJP <6775@holbertonstudents.com>
2023-09-25 09:27:54 -0700

    10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,8 @@ def best_score(a_dictionary):
     best_key = None
     best_value = None
     for key, value in a_dictionary.items():
-        if isinstance(value, int) and (best_value is None or value > best_value):
-            best_value = value
-            best_key = key
+        if isinstance(value, int):
+            if (best_value is None or value > best_value):
+                best_value = value
+                best_key = key
     return best_key
