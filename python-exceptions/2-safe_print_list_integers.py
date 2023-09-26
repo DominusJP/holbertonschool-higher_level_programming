10ea199434ec8f45ac35de50b62eb93152852244
DominusJP <6775@holbertonstudents.com>
2023-09-26 07:45:18 -0700

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in range (x):
+        for i in range(x):
             element = my_list[i]
-            if isinstance(element, int ):
+            if isinstance(element, int):
                 print("{:d}".format(element), end="")
                 count += 1
     except IndexError:
