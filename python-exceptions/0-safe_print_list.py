826a6d244ac1e61c5c881a6891f4fe4029d5e735
DominusJP <6775@holbertonstudents.com>
2023-09-26 06:49:34 -0700

    0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             element = my_list[i]
-            print(element, end=" ")
+            print(element, end="")
             count += 1
     except IndexError:
         pass
