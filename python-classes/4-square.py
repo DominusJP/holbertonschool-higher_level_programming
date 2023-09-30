b14d70c392299b9276fefea756e9574645e964d6
DominusJP <6775@holbertonstudents.com>
2023-09-30 11:52:46 -0700

    4

---
+++ python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Empezamos con los comentarios/we start with the comments"""
+
+
+class Square:
+    """
+    Does this work like this?
+    """
+    def __init__(self):
+        return self.__size
+
+    def size(self, value)
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
