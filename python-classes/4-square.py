32a1ff70607383e76c99c6dd8a51cf5fec0d98bd
DominusJP <6775@holbertonstudents.com>
2023-09-30 12:05:35 -0700

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,24 +4,23 @@
 
 class Square:
     """
-    Does this work like this?
+    project #3 creating a class to create a square
     """
-    def __init__(self):
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
         return self.__size
 
-    """
-    adding comments
-    """
-    def size(self, value)
-    if not isinstance(value, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    else:
-        self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
-    """
-    is a comment needed here as well?
-    """
     def area(self):
         return self.__size ** 2
