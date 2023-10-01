d67a912f447be839295941ab9373edee9e43d2eb
DominusJP <6775@holbertonstudents.com>
2023-10-01 15:50:19 -0700

    -3

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_square(size):
+    if not isinstance(size, int):
+        if isinstance(size, float) and size.is_integer():
+            size = int(size)
+        else:
+            raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
