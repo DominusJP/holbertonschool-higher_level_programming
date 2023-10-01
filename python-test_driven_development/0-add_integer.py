94061e7d87c015662c3bf52a0c4382d12bff44b2
DominusJP <6775@holbertonstudents.com>
2023-10-01 15:00:47 -0700

    running in the dark with this goddamn checker

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,10 +6,10 @@ def add_integer(a, b=98):
     """
     adding two integers
     """
-    if (isinstance(a, int) or isinstance(a, float)):
-        if (isinstance(b, int) or isinstance(b, float)):
-            a = int(a)
-            b = int(b)
-            return a + b
-    else:
-        raise TypeError("a must be an integer or b must be an integer")
+    if not isinstance(a, int) and not isinstance(a, float):
+    	raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
