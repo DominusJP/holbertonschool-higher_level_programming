e5e08416f60f9444adf81b6aa51ff4a05b6f897f
DominusJP <6775@holbertonstudents.com>
2023-10-04 11:11:56 -0700

    0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Empezamos con los comentarios/we start with the comments
+Modulo, Python - More classes. Time to code a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class if for a rectangle
+    """
+    pass
