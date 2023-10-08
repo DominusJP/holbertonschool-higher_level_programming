c35c4ebfd303daeb782c6252b68ef864f39d43aa
DominusJP <6775@holbertonstudents.com>
2023-10-08 13:32:06 -0700

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""input and output module"""
+
+
 def read_file(filename=""):
+    """function to print something from a file"""
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             for line in file:
