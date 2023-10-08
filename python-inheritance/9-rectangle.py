1d8a75b02b84e7948ee3c5c797695600ef4ed340
DominusJP <6775@holbertonstudents.com>
2023-10-07 19:43:02 -0700

    9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""querido python inheritance"""
+"""rectangle time baby"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class  that will inheit its shit from geometrybase"""
+    """class Rectangle that inherits
+    from BaseGeometry"""
     def __init__(self, width, height):
-	"""rectangle maker"""
+        """constructor of rectangle"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """return the freaking area"""
+        """return the area of the rectangle"""
         return self.__width * self.__height
 
     def __str__(self):
