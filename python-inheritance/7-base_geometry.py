ab2181813fd91d40802a112698ebbcfb6853a2b4
DominusJP <6775@holbertonstudents.com>
2023-10-07 19:22:44 -0700

    7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,7 +13,9 @@ class BaseGeometry:
         """
         Validates the value for being an integer and greater than 0.
         """
+        if isinstance(value, int):
+            self.name = name
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError(f"{self.name} must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(f"{self.name} must be greater than 0")
