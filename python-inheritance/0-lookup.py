d4f0af84dd078f2a850324cd1b7b48ecb21d417b
DominusJP <6775@holbertonstudents.com>
2023-10-07 18:30:01 -0700

    y los coments

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -5,4 +5,10 @@ Python inheritance module
 
 
 def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    :param obj: The object to inspect.
+    :return: A list of attributes and methods.
+    """
     return dir(obj)
