37391099cfc0a1904e01a20fdcac4e388e4bfa73
DominusJP <6775@holbertonstudents.com>
2023-10-07 19:16:09 -0700

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" weiiiiiiiiii"""
+
+
+class BaseGeometry:
+    """not soooooooooooooo empty class"""
+    def area(self):
+        raise Exception("area() is not implemented")
