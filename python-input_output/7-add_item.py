eef1ee3422c6758b63576770842c224c587f1b66
DominusJP <6775@holbertonstudents.com>
2023-10-08 18:38:56 -0700

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,10 +6,19 @@ import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-try:
-    existing_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    existing_list = []
-arguments = sys.argv[1:]
-existing_list.extend(arguments)
-save_to_json_file(existing_list, "add_item.json")
+# Check if any command-line arguments are provided
+if len(sys.argv) > 1:
+    # Try to load the existing JSON data from the file
+    try:
+        existing_data = load_from_json_file("add_item.json")
+    except (FileNotFoundError, json.JSONDecodeError):
+        existing_data = []
+
+    # Add the command-line arguments to the existing data
+    existing_data.extend(sys.argv[1:])
+
+    # Save the updated data to the file
+    save_to_json_file(existing_data, "add_item.json")
+    print("Arguments added and saved to 'add_item.json'.")
+else:
+    print("No arguments provided. Nothing to add.")
