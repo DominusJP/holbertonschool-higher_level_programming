5005fb4b9e6834ad7a91b2451efe3b9a1ee64875
DominusJP <6775@holbertonstudents.com>
2023-10-08 18:43:32 -0700

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-"""input and output module"""
-import json
+"""more about this project, input output"""
 
 
 def class_to_json(obj):
-    """Create a dictionary to hold the serialized data"""
+    """comentando la clase"""
     serialized_data = {}
     for attr_name in dir(obj):
+        # Skip private attributes and methods (those starting with an underscore)
         if attr_name.startswith("_"):
             continue
+        
+        # Get the attribute value
         attr_value = getattr(obj, attr_name)
-        if isinstance(attr_value, (list, dict, str, int, bool)):
+        
+        # Check if the attribute value is serializable
+        if is_serializable(attr_value):
             serialized_data[attr_name] = attr_value
+
     return serialized_data
+
+def is_serializable(value):
+    """Check if a value is serializable
+    (list, dict, str, int, or bool)"""
+    return isinstance(value, (list, dict, str, int, bool))
