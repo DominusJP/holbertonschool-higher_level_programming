7076201c749fa0fdb524fdd54229c746ab5de72a
DominusJP <6775@holbertonstudents.com>
2023-10-08 18:45:18 -0700

    8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,18 +6,13 @@ def class_to_json(obj):
     """comentando la clase"""
     serialized_data = {}
     for attr_name in dir(obj):
-        # Skip private attributes and methods (those starting with an underscore)
         if attr_name.startswith("_"):
             continue
-        
-        # Get the attribute value
         attr_value = getattr(obj, attr_name)
-        
-        # Check if the attribute value is serializable
         if is_serializable(attr_value):
             serialized_data[attr_name] = attr_value
-
     return serialized_data
+
 
 def is_serializable(value):
     """Check if a value is serializable
