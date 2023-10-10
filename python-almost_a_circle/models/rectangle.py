bb1feecd80ca9354c2eb179dbc3a5a05cadd9849
DominusJP <6775@holbertonstudents.com>
2023-10-10 08:49:50 -0700

    6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,8 +71,10 @@ class Rectangle(Base):
 
     def display(self):
         """prints in stdout the Rectangle instance"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
