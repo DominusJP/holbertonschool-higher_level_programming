810da8deda7e284de72cf6ab117b71610c21d0ca
DominusJP <6775@holbertonstudents.com>
2023-10-14 12:35:54 -0700

    8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,17 @@ class Rectangle(Base):
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
                f"{self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """Assigns arguments to attributes in the specified order."""
+        arg_count = len(args)
+        if arg_count >= 1:
+            self.id = args[0]
+        if arg_count >= 2:
+            self.width = args[1]
+        if arg_count >= 3:
+            self.height = args[2]
+        if arg_count >= 4:
+            self.x = args[3]
+        if arg_count >= 5:
+            self.y = args[4]
