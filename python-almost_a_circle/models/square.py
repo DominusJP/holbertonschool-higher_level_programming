7b9eb0f2ebb51f7e4ed146598c61a67284abd5ed
DominusJP <6775@holbertonstudents.com>
2023-10-14 13:48:09 -0700

    11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,21 @@ class Square(Rectangle):
         """init function"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """getting the good stuf"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setting the good stuf"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """defining self"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
