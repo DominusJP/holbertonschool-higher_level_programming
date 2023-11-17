a87d20679346000df69f622619ea1f997c1f2250
DominusJP <6775@holbertonstudents.com>
2023-11-17 10:09:24 -0800

    9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,5 @@
 --  script that lists all cities contained in the database
 --  query to list all cities
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id;
