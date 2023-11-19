22af200137ef51e352c73e745fa3105194891ac2
DominusJP <6775@holbertonstudents.com>
2023-11-18 19:56:09 -0800

    0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that lists all states from the database hbtn_0e_0_usa
+
+"""
+import MySQLdb
+import sys
+
+
+def list_states(username, password, dbname):
+    db = MySQLdb.connect(user=username, passwd=password, db=dbname, host='localhost', port=3306)
+    cursor = db.cursor()
+    query = "SELECT * FROM states ORDER BY id ASC;"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    db.close()
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <username> <password> <database>")
+        sys.exit(1)
+
+    username, password, dbname = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, dbname)
